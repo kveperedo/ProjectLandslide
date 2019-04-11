aec276f46ade7e6e14b7043f226c6b21443e4184
kevin peredo <kveperedo@gmail.com>
2019-04-11 21:52:22 +0800

    Major Revision v1 (Navigation, Windows, etc)

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-let convertFahrenheits = (fahren) => {
-    let celsius = (fahren - 32) * 5 / 9
-    let kelvin = (fahren + 459.67) * 5 / 9
+let mainDashboard = document.getElementById('mainDashboard')
+let mainHome = document.getElementById('mainHome')
+let sideHome = document.getElementById('sideHome')
+let sideDash = document.getElementById('sideDash')
+let sideAcc = document.getElementById('sideAcc')
+let sideSettings = document.getElementById('sideSettings')
 
-    return {
-        fahrenheitValue: fahren,
-        celsiusValue: celsius,
-        kelvinValue: kelvin, 
+let sideParent = document.getElementById('sideBar')
+
+sideParent.addEventListener('click', function (side) {
+    if (side.target !== side.currentTarget) {
+        document.querySelector(".sideEffect").classList.remove("sideEffect")
+        side.target.classList.add('sideEffect')
     }
-}
+    side.stopPropagation()
+})
 
-console.log(convertFahrenheits(50))
+sideHome.addEventListener('click', function () {
+    mainHome.style.display = 'block'
+    mainDashboard.style.display = 'none'
+    mainAcc.style.display = 'none'
+    mainSettings.style.display = 'none'
+})
+
+sideDash.addEventListener('click', function () {
+    mainHome.style.display = 'none'
+    mainDashboard.style.display = 'block'
+    mainAcc.style.display = 'none'
+    mainSettings.style.display = 'none'
+})
+
+sideAcc.addEventListener('click', function () {
+    mainHome.style.display = 'none'
+    mainDashboard.style.display = 'none'
+    mainAcc.style.display = 'block'
+    mainSettings.style.display = 'none'
+})
+
+sideSettings.addEventListener('click', function () {
+    mainHome.style.display = 'none'
+    mainDashboard.style.display = 'none'
+    mainAcc.style.display = 'none'
+    mainSettings.style.display = 'block'
+})
